21b6c9a2b9700c9a2c3254bd19c4b92be25b9517
Bartłomiej Rzepka <bartlomiejrzepka@wp.pl>
2022-04-02 18:49:31 +0200

    feat: navigation and dashboard added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,94 +1,20 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Chart } from 'chart.js';
-import HeatMap from 'heatmap-ts';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('testChartCanvas') testChartCanvas!: ElementRef<HTMLCanvasElement>;
-  @ViewChild('testHeatMapDiv') testHeatMapDiv!: ElementRef<HTMLDivElement>;
+export class AppComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
 
-  ngAfterViewInit(): void {
-    const ctx = this.testChartCanvas.nativeElement.getContext('2d');
-    if (ctx) this.generateExampleChart(ctx);
-
-    const heatMap = new HeatMap({
-      container: this.testHeatMapDiv.nativeElement,
-      maxOpacity: 0.6,
-      radius: 50,
-      blur: 0.9,
-      width: 300,
-      height: 300,
-    });
-    this.generateExampleHeatMap(heatMap);
-  }
-
-  generateExampleHeatMap(heatMap: HeatMap): void {
-    // screw 'vars' for now...
-    const points = [];
-    let max = 0;
-    const width = 840;
-    const height = 400;
-    let len = 200;
-
-    while (len--) {
-      const val = Math.floor(Math.random() * 100);
-      max = Math.max(max, val);
-      const point = {
-        x: Math.floor(Math.random() * width),
-        y: Math.floor(Math.random() * height),
-        value: val,
-      };
-      points.push(point);
-    }
-
-    const data = {
-      max: max,
-      data: points,
-    };
-
-    heatMap.setData(data);
-  }
-
-  generateExampleChart(ctx: CanvasRenderingContext2D): void {
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestingComponent } from './components/testing/testing.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { ExampleChartComponent } from './components/example-chart/example-chart.component';
+
+const CDK_MODULES = [LayoutModule];
+
+const MATERIAL_MODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TestingComponent, DashboardComponent, ExampleChartComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ...CDK_MODULES,
+    ...MATERIAL_MODULES,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
