30c0b81aebdbd1b1d3877b6b6e24e3a56d00093c
pyrkoszzz <pepexd555@gmail.com>
2022-04-02 17:09:29 +0200

    working qr_scanner-app

---
+++ mobile_app/screens/TabOneScreen.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+import Cam from '../components/Camera'
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <Cam />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
