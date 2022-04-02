daaa3379c5ff9646f1c7ed8963cae310cfa58473
Ezarus12 <98115422+Ezarus12@users.noreply.github.com>
2022-04-03 00:37:59 +0200

    Add files via upload

--- backend/API/models.py
+++ backend/API/models.py
@@ -1,3 +1,4 @@
+from platform import machine
 from django.db import models
 from django.utils import timezone
 
@@ -9,6 +10,7 @@ class User(models.Model):
     nick = models.CharField(max_length=30)
     phone_number = PhoneNumberField(null=False, blank=False, unique=True)
     allergies = models.JSONField(null=True)
+    user_cat = models.IntegerField()
 
 
 class Heatmap(models.Model):
@@ -19,21 +21,38 @@ class Heatmap(models.Model):
 class StoreProduct(models.Model):
     name = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
+    allergies = models.JSONField(null=True)
+    price = models.FloatField()
 
 
 class FactoryProduct(models.Model):
     name = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
+    serial_num = models.IntegerField()
 
 
 class Machine(models.Model):
     name = models.CharField(max_length=30)
-
+    id = models.ForeignKey(machine, on_delete= models.SET_NULL, NULL = True)
+    workingtime: datetime.datetime = models.DateTimeField(default=timezone.localtime)
+    end_date = models.DateField()
+    service_date = models.DateField()
+    
 
 class StoreCategories(models.Model):
     name = models.CharField(max_length=30)
+    demand = models.IntegerField()
+
 
 
 class FactoryCategory(models.Model):
     name = models.CharField(max_length=30)
 
+
+class StatStore(models.Model):
+    product_count = models.IntegerField()
+    date = models.DateField()
+
+class StatFactory(models.Model):
+    num_of_errors = models.IntegerField()
+    date = models.DateField()
